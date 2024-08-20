568c13431e3a4ac7149620b5261c6ea712859396
Markedia Hinds <mhinds@pursuit.org>
2024-08-20 06:01:41 -0400

    Resolve Tamagotchi error, file was outside of folder.  Constructor and 3 methods effectively functioning: greet(), status(), eat().

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,40 @@
-// Create class below
+class Tamagotchi {
+    constructor (name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false) {
+        this.name = name
+        this.energy = energy
+        this.full = full
+        this.mood = mood
+        this.sick = sick
+        this.rehomed = rehomed
+    }
 
-// Do not edit below this line
+    greet () {
+        console.log(`Hello, I'm ${this.name}`)
+    }
+
+    status () {
+        console.log(`My mood is: ${this.mood} \n I am this full: ${this.full} \n My nrg is: ${this.energy} \n I am well` )
+    }
+
+    eat () {
+        this.full+=2
+        this.energy--
+        if ( this.full > 10 ) {
+            this.sick = true
+        }
+    }
+}
+
+
+const love = new Tamagotchi('Love')
+// console.log(love)
+// love.greet()
+// love.status()
+// console.log(love)
+// love.eat()
+// console.log(love)
+// love.eat()
+console.log(love)
+
+
 module.exports = Tamagotchi;
