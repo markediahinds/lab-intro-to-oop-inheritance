848c7540261030beee2798b33e390fd5df762526
Markedia Hinds <mhinds@pursuit.org>
2024-08-20 05:52:08 -0400

    Food file complete, troubleshooting Tamagotchi reference errors

--- food.js
+++ food.js
@@ -1,4 +1,41 @@
 // Create class below
+class Food {
+    constructor (name, daysToSpoil, fresh) {
+        this.name = name
+        this.daysToSpoil = daysToSpoil
+        this.fresh = fresh
+    }
 
-// Do not edit below this line
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+
+    isFresh() {
+        if ( this.daysToSpoil >= 1 ) {
+            console.log(`There are ${this.daysToSpoil} days left before the ${this.name} spoils.`)
+        } else {
+            console.log(`${this.name} has spoiled.`)
+            this.fresh = false 
+        }
+    }
+
+    aDayPasses() {
+            this.daysToSpoil--
+            this.isFresh()
+        
+    }
+}
+
+const mango = new Food('mango', '2', true)
+const pear = new Food('pear', '0', false)
+
+// mango.prepare()
+// // mango.isFresh()
+// console.log(mango)
+// mango.aDayPasses()
+// console.log(mango)
+// mango.aDayPasses()
+console.log(mango)
+
+
 module.exports = Food;
